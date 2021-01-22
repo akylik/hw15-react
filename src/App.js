1400a3b569ecfbfdce7814ff2c6b6b7a1ec99b85
akylik <a_kylik@ukr.net>
2021-01-22 17:37:04 +0200

    fixed deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React from 'react';
+
 import Post from './component/template.js';
+
+import './App.css';
 import './component/template.css';
 
-const pData = [
+const postData = [
    { 
      name: "Anakin Skywalker",
      photo: "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg",
@@ -17,58 +17,14 @@ const pData = [
      reTweet: 146,
      like: 887    
    },
-   // { 
-   //   name: "Emperor Palpatine",
-   //   photo: "./img/sheevPalpatine-ava.jpg",
-   //   nickname: "@sheva-imperator",
-   //   date: "17 apr.",   
-   //   content: "That woman could turn any Ewok into a Wookie.", 
-   //   image: "./img/sw9_PadmeAmidala-pictol.jpg",    
-   //   message: 321,
-   //   reTweet: 97,
-   //   like: 784    
-   // },
-   // { 
-   //   name: "Princess/General Leia Organa",
-   //   photo: "./img/leiaOrgana-ava.jpg",
-   //   nickname: "@princess-Leia",
-   //   date: "25 sep.",    
-   //   content: "What if you took Han Solo and gave him no depth?",
-   //   image: "./img/sw9_PoeDameron-jet.jpg",    
-   //   message: 296,
-   //   reTweet: 138,
-   //   like: 754    
-   // }
  ];
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App() {
    return (
      <div className="App">
-       { pData.map(post => <Post {...post} /> )}
+       { postData.map(post => <Post {...post} /> )}
      </div>
    );
  }
-
 
 export default App;
