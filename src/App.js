39efd422cdb4e60a857ac696d5797e5fc9297bf6
Kulyk Oleksandr <70694467+akylik@users.noreply.github.com>
2021-06-17 15:21:12 +0300

    fixed image url

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import './App.css';
 import './component/template.css';
 
 const postData = [
-   { 
-     name: "Anakin Skywalker",
-     photo: "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg",
-     nickname: "@dart_vader",
-     date: "26 февр.",    
-     content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
-     image: "./img/sw9_ReySabre-laser.jpg",    
-     message: 482,
-     reTweet: 146,
-     like: 887    
-   },
- ];
+  {
+    name: "Anakin Skywalker",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg",
+    nickname: "@dart_vader",
+    date: "26 февр.",
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image:
+      "https://github.com/akylik/hw20-react/blob/gh-pages/img/sw9_ReySabre-laser.jpg?raw=true",
+    message: 482,
+    reTweet: 146,
+    like: 887,
+  },
+];
 
 function App() {
    return (
--- src/component/template.js
+++ src/component/template.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Post = (props) => {
   return (
     <div className="card">
-
       <div className="card__header">
         <span className="card__header_avatar">
           <img src={props.photo} />
@@ -11,14 +10,26 @@ const Post = (props) => {
         <span className="card__header__textBox">
           <div className="card__header__textBox_name">
             <span className="bold">{props.name}</span>
-            <img src='./img/star-active.png' className="star-active" alt="Active Button" />         
-            <span className="gray"><a>{props.nickname}</a> • {props.date}</span> 
-          </div>       
+            <img
+              src="https://github.com/akylik/hw20-react/blob/gh-pages/img/star-active.png?raw=true"
+              className="star-active"
+              alt="Active Button"
+            />
+            <span className="gray">
+              <a>{props.nickname}</a> • {props.date}
+            </span>
+          </div>
           <div className="card__header__textBox_status">
             <span>{props.content}</span>
-          </div>          
+          </div>
         </span>
-        <span className="card__header__textBox_dw_Arrow"><img src="./img/down-arrow.png" width="14px" alt="Down Arrow"/></span>
+        <span className="card__header__textBox_dw_Arrow">
+          <img
+            src="https://github.com/akylik/hw20-react/blob/gh-pages/img/down-arrow.png?raw=true"
+            width="14px"
+            alt="Down Arrow"
+          />
+        </span>
       </div>
 
       <div className="card__center">
@@ -26,21 +37,44 @@ const Post = (props) => {
       </div>
 
       <div className="card__footer">
-      <span>
-        <a src="#"><img src="./img/tw-message.png" alt="Message Icon"/><span className="icon2text">{props.message}</span></a>
-      </span>
-      <span className="leftTab">
-        <a src="#"><img src="./img/tw-retweet.png" alt="Retweet Icon"/><span className="icon2text">{props.reTweet}</span></a>
-      </span>
-      <span className="leftTab">
-        <a src="#"><img src="./img/tw-like.png" alt="Like Icon"/><span className="icon2text">{props.like}</span></a>
-      </span>
-      <span className="leftTab">
-        <a src="#"><img src="./img/tw-share.png" alt="Share Icon" /></a>
-      </span>
+        <span>
+          <a src="#">
+            <img
+              src="https://github.com/akylik/hw20-react/blob/gh-pages/img/tw-message.png?raw=true"
+              alt="Message Icon"
+            />
+            <span className="icon2text">{props.message}</span>
+          </a>
+        </span>
+        <span className="leftTab">
+          <a src="#">
+            <img
+              src="https://github.com/akylik/hw20-react/blob/gh-pages/img/tw-retweet.png?raw=true"
+              alt="Retweet Icon"
+            />
+            <span className="icon2text">{props.reTweet}</span>
+          </a>
+        </span>
+        <span className="leftTab">
+          <a src="#">
+            <img
+              src="https://github.com/akylik/hw20-react/blob/gh-pages/img/tw-like.png?raw=true"
+              alt="Like Icon"
+            />
+            <span className="icon2text">{props.like}</span>
+          </a>
+        </span>
+        <span className="leftTab">
+          <a src="#">
+            <img
+              src="https://github.com/akylik/hw20-react/blob/gh-pages/img/tw-share.png?raw=true"
+              alt="Share Icon"
+            />
+          </a>
+        </span>
       </div>
     </div>
-  )
+  );
 }
 
 export default Post;
